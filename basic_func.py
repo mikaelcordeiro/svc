eea8cb1f448014f5638eaefd19bafc2413f4c472
mikaelvm <mikaelcordeiro@usp.br>
2021-08-02 20:45:39 -0300

    termina funcoes basicas com translacoes, rotacoes e flip

--- basic_func.py
+++ basic_func.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 imagem = cv.imread(filename='/home/mikael/scripts/python/svc/fotos/pessoas2.jpg')
 
@@ -59,5 +60,43 @@ pixel_x_inicial, pixel_x_final = 200, 400
 cropped = imagem[pixel_y_inicial:pixel_y_final, pixel_x_inicial:pixel_x_final]
 
 cv.imshow('imagem cortada', cropped)
+
+'''============================================ translac imagem =================================================='''
+
+x = 100  # positivo -> direita, negativo -> esquerda (em pixels)
+
+y = 100  # positivo -> baixo, negativo -> cima (em pixels)
+
+matriz_translac = np.float32([[1, 0, x], [0, 1, y]])
+
+img_translac = cv.warpAffine(src=imagem, M=matriz_translac, dsize=(dimensao[1], dimensao[0]))
+
+cv.imshow('imagem translacao', mat=img_translac)
+
+'''============================================ rot imagem =================================================='''
+
+ponto_rot = (dimensao[1]//2, dimensao[0]//2)  # centro da imagem sera o ponto de rot
+
+angulo = 45  # positivo -> anti-clock, negativo -> clock (em graus)
+
+matriz_rot = cv.getRotationMatrix2D(center=ponto_rot, angle=angulo, scale=1.)
+
+img_rot = cv.warpAffine(src=imagem, M=matriz_rot, dsize=(dimensao[1], dimensao[0]))
+
+cv.imshow('imagem rot', mat=img_rot)
+
+matriz_rot_2 = cv.getRotationMatrix2D(center=ponto_rot, angle=-angulo, scale=1.)
+
+img_rot_rot = cv.warpAffine(src=imagem, M=matriz_rot_2, dsize=(dimensao[1], dimensao[0]))
+
+cv.imshow('imagem rotacionda da rotacao', mat=img_rot_rot)  # caso suma algum pedaco da imagem, e que foi perdido na 1 rotacao
+
+'''============================================ flip imagem =================================================='''
+
+codigo = -1 # 0 -> eixo "x", 1 -> eixo "y", -1 -> os dois 
+
+img_flipped = cv.flip(src=imagem, flipCode=codigo)
+
+cv.imshow('flip da imagem', mat=img_flipped)
 
 cv.waitKey(0)
